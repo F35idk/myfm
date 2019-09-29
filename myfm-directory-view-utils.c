c41c23fd80fb2542cb0cca1cc5ee1712106b209c
F35 <f35idk@gmail.com>
2019-09-29 14:45:52 +0200

    inline useless function and improve comments

--- myfm-directory-view-utils.c
+++ myfm-directory-view-utils.c
@@ -9,40 +9,7 @@
 #include "myfm-file.h"
 #include "myfm-directory-view.h"
 
-/* welcome to callback hell. function order of execution:
-*     files_to_store_async --> g_file_enumerate_children_async
-* --> enum_files_callback --> g_file_enumerator_next_files_async
-* --> next_files_callback */
-
 static gint num_files = 32; // TODO: HOW MANY?????
-
-static void add_files (GtkListStore *store, GFile *parent_file, GList *file_list)
-{
-    GtkTreeIter iter;
-    GFileInfo *child_info; /* doesn't need autoptr */
-    GError *error = NULL; /* doesn't need autoptr */
-    GList *current_node = file_list;
-
-    while (current_node) {
-        child_info = current_node->data;
-        const char *child_name = g_file_info_get_display_name (child_info); // TODO: no free
-        GFile *child_g_file = g_file_get_child_for_display_name (parent_file, child_name, &error);
-
-        if (error) {
-            g_object_unref (child_info);
-            g_object_unref (child_g_file);
-            g_error_free (error);
-            error = NULL; /* TODO: KEEP TABS */
-        }
-        else {
-            MyFMFile *child_myfm_file = myfm_file_new_without_io (child_g_file, g_strdup (child_name));
-            gtk_list_store_append (store, &iter); /* out iter */
-            gtk_list_store_set (store, &iter, 0, (gpointer) child_myfm_file, -1);
-            g_object_unref (child_info);
-        }
-        current_node = current_node->next;
-    }
-}
 
 /* https://stackoverflow.com/questions/35036909/c-glib-gio-how-to-list-files-asynchronously
  * function calls itself until all files have been listed */
@@ -62,9 +29,33 @@ static void next_files_callback (GObject *file_enumerator, GAsyncResult *result,
         return;
     }
     else { /* enumerator returned successfully */
-        GFile *dir = myfm_directory_view_get_directory (MYFM_DIRECTORY_VIEW (dirview))->g_file;
+        GtkTreeIter iter;
+        GFileInfo *child_info; /* doesn't need autoptr */
+        GList *current_node = directory_list;
+        GFile *parent_dir = myfm_directory_view_get_directory (MYFM_DIRECTORY_VIEW (dirview))->g_file;
         GtkListStore *store = GTK_LIST_STORE (gtk_tree_view_get_model (GTK_TREE_VIEW (dirview)));
-        add_files (store, dir, directory_list);
+
+        /* iterate over directory_list and add files to store */
+        while (current_node) {
+            child_info = current_node->data;
+            const char *child_name = g_file_info_get_display_name (child_info); // TODO: no free
+            GFile *child_g_file = g_file_get_child_for_display_name (parent_dir, child_name, &error);
+
+            if (error) {
+                g_object_unref (child_info);
+                g_object_unref (child_g_file);
+                g_error_free (error);
+                error = NULL; /* TODO: KEEP TABS */
+            }
+            else {
+                MyFMFile *child_myfm_file = myfm_file_new_without_io (child_g_file, g_strdup (child_name));
+                gtk_list_store_append (store, &iter); /* out iter */
+                gtk_list_store_set (store, &iter, 0, (gpointer) child_myfm_file, -1);
+                g_object_unref (child_info);
+            }
+            current_node = current_node->next;
+        }
+        /* continue calling next_files and next_files_callback recursively */
         g_file_enumerator_next_files_async (G_FILE_ENUMERATOR (file_enumerator),
                                            num_files, G_PRIORITY_HIGH, NULL,
                                            next_files_callback, dirview);
@@ -92,7 +83,11 @@ static void enum_finished_callback (GObject *directory, GAsyncResult *result, gp
     }
 }
 
-/* adds the files in a directory to the dirview's list store */
+/* adds the files in a directory to the dirview's list store. also sets off
+ * a chain of crazy async functions and callbacks. order of execution is:
+ *     files_to_store_async --> g_file_enumerate_children_async
+ * --> enum_files_callback --> g_file_enumerator_next_files_async
+ * --> next_files_callback. welcome to callback hell */
 void files_to_store_async (MyFMDirectoryView *dirview)
 {
     g_file_enumerate_children_async (myfm_directory_view_get_directory (dirview)->g_file,
@@ -103,6 +98,7 @@ void files_to_store_async (MyFMDirectoryView *dirview)
                                      dirview);
 }
 
+/* unrefs and sets to null a single file in a list store */
 gboolean clear_file_in_store (GtkTreeModel *model, GtkTreePath *path, GtkTreeIter *iter, gpointer data)
 {
     gpointer myfm_file;
