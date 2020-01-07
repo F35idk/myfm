fe012bd29e80f90a179f442de30871867d85b5b8
F35 <f35idk@gmail.com>
2020-01-07 12:24:00 +0100

    begin implementing (WIP) move operation

---
+++ myfm-move-operation.h
@@ -0,0 +1,18 @@
+/*
+ * Created by f35 on 04.01.20.
+*/
+
+#ifndef __MYFM_MOVE_OPERATION_H
+#define __MYFM_MOVE_OPERATION_H
+
+#include <gtk/gtk.h>
+
+#include "myfm-file.h"
+#include "myfm-window.h"
+
+typedef void (*MyFMMoveCallback)(gpointer);
+
+void myfm_move_operation_start_async (MyFMFile **src_files, gint n_files, MyFMFile *dest_dir,
+                                      GtkWindow *active, MyFMMoveCallback cb, gpointer data);
+
+#endif /* __MYFM_MOVE_OPERATION_H */
